0ec6a825ae395799cbb2587c188be5746932dde0
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-11-08 00:45:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 'use strict';
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
+
   register(/*{ strapi }*/) {},
 
   /**
