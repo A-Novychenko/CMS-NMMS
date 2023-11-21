3acc799071d0a8ddce2c46ca2f96090032e7cfa9
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-11-22 01:44:52 +0300

    Update about-massage.js

--- src/api/about-massage/controllers/about-massage.js
+++ src/api/about-massage/controllers/about-massage.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * about-massage controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::about-massage.about-massage');
