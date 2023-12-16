7eb08bed5b8a6fa107bb19bca9e710a12ad24f64
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-12-17 00:42:12 +0200

    Update news-post.js

--- src/api/news-post/services/news-post.js
+++ src/api/news-post/services/news-post.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * news-post service
- */
-
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::news-post.news-post');
