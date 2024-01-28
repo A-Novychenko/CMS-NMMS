41c55f4df5e67feb4857f9a21f7204c79a3b0b20
Aleksandr Novychenko <A.Novychenko@outlook.com>
2024-01-28 20:36:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ module.exports = {
 
   register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets starte
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
+
   bootstrap(/*{ strapi }*/) {},
 };
